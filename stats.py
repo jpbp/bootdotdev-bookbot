bfa25ce62633d9e92b35eeed12b236e2342b41d2
jpbp <jppenna04@gmail.com>
2025-05-20 11:47:18 -0300

    Adicionado a função get_number_of_times_each_character onde ela irá retornar um dicionario contando quantas vezes cada caracter apareceu

--- stats.py
+++ stats.py
@@ -8,3 +8,14 @@ def get_count_of_words_text(book):
     words = book.split()
     count_words = len(words)
     return count_words 
+
+def get_number_of_times_each_character(book):
+    characters = {}
+    book = book.lower()
+    for char in book:
+        if (not char in characters):
+            characters[char] = 1
+        else:
+            characters[char] += 1
+    return characters
+    
